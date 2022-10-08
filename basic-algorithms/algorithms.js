08c610628c2cddc44a306284c1140080d15f8554
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-08 18:54:49 +0200

    add Iteration#4

--- basic-algorithms/algorithms.js
+++ basic-algorithms/algorithms.js
@@ -6,88 +6,125 @@ let myFavoriteHero = "Hulk";
 // 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 let x = 50;
 
-// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
 let h = 5;
 let y = 10;
 
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y;
-let vicky =  30;
+let vicky = 30;
 
 
+//--------------------------------------------------------------------------------
 
 // **Iteración #2: Variables avanzadas**
+
+
 // 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
-const character = {name: 'Jack Sparrow', age: 10};
+const character = { name: "Jack Sparrow", age: 10 };
 character.age = 25;
 
-// 1.2 Declara 3 variables con los nombres y valores siguientes 
-// 	firstName = 'Jon'; 
-// 	lastName = 'Snow'; 
-// 	age = 24; 
-// Muestralos por consola de esta forma: 
+// 1.2 Declara 3 variables con los nombres y valores siguientes
+// 	firstName = 'Jon';
+// 	lastName = 'Snow';
+// 	age = 24;
+// Muestralos por consola de esta forma:
 // 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
 
-firstName = 'Luisfer';
-lastName = 'Guevara'; 
-age = '25';
+firstName = "Luisfer";
+lastName = "Guevara";
+age = "25";
 console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gusta la programación`);
-
 
 // 1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 // ambos juguetes.
-const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
+const toy1 = { name: "Buss myYear", price: 19 };
+const toy2 = { name: "Rallo mcKing", price: 29 };
 
 console.log(toy1.price + toy2.price);
+
 // 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a
- //25000 y actualiza la propiedad finalPrice de todos los coches con el valor de su 
- //propiedad 
-// basePrice más el valor de la variable globalBasePrice.
-// let globalBasePrice = 10000;
-// const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-// const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
-// ```
+//25000 y actualiza la propiedad finalPrice de todos los coches con el valor de su
+//propiedad basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = { name: "BMW m&m", basePrice: 50000, finalPrice: 60000 };
+const car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
+
+globalBasePrice = 2500;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
+
+
+//--------------------------------------------------------------------------------
+
+
 
 // **Iteración #3: Operadores**
 
-// ```jsx
-// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
 
-// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log.
+let numA = 10; 
+console.log(numA / 5);
 
-// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.log.
+console.log(numA / 2);
 
-// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// 1.3 Muestra mediante un console.log el resto de dividir 15 por 9.
+let numB = 15; 
+console.log(numB % 9);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15,
 // teniendo dos variables y = 10 y z = 5.
+y = 10;
+z = 5;
+x =  y + z;
+
 
 // 1.5 Usa el correcto operador de asignación que resultará en x = 50,
 // teniendo dos variables y = 10 y z = 5.
+x = y * z;
 
-// ```
+
+//--------------------------------------------------------------------------------
+
+
+
 
 // **Iteración #4: Arrays**
 
-// ```jsx
 // 1.1 Consigue el valor "HULK" del array de cars y muestralo por consola.
-// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
 
 // 1.2 Cambia el primer elemento de avengers a "IRONMAN"
-// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers[0] = 'IRONMAN'
 
-// 1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
-// const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+// 1.3 Console.log numero de elementos en el array usando la propiedad correcta de Array.
+console.log(avengers.length)
 
-// 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+// 1.4 Añade 2 elementos al array: "Morty" y "Summer".
 // Muestra en consola el último personaje del array
-// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push('Morty', 'Summer');
+console.log(rickAndMortyCharacters);
 
 // 1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
-// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", 
+"Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2[0], 'and', 
+    rickAndMortyCharacters2[rickAndMortyCharacters2.length-1]);
+
 
 // 1.6 Elimina el segundo elemento del array y muestra el array por consola.
-// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
-// ```
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", 
+"Lapiz Lopez"];
+rickAndMortyCharacters3.splice(1,1);
+console.log(rickAndMortyCharacters3);
+
+
+
+//--------------------------------------------------------------------------------------
 
 // **Iteración #5: Condicionales**
 
@@ -128,16 +165,20 @@ console.log(toy1.price + toy2.price);
 // }
 // ```
 
+
+
+//--------------------------------------------------------------------------------------
+
 // **Iteración #6: Bucles**
 
 // ```jsx
 // 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
 
-// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo
 // cuando el resto del numero dividido entre 2 sea 0.
 
-// 1.3 Crea un bucle para conseguir dormir contando ovejas. 
-// Este bucle empieza en 0 y termina en 10. 
-// Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+// 1.3 Crea un bucle para conseguir dormir contando ovejas.
+// Este bucle empieza en 0 y termina en 10.
+// Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle
 // y cambia el mensaje en la última vuelta a 'Dormido!'.
 // ```
