110e4e60d4c97bb8c5622ac39b3a30e1ced0b7e5
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-08 19:35:30 +0200

    FEAT: add basic-functions and add Iterations#1

---
+++ basic-functions/1.maximun.js
@@ -0,0 +1,15 @@
+// **Iteración #1: Buscar el máximo**
+
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+   if(numberOne > numberTwo){
+    return numberOne
+   }else if(numberTwo > numberOne){
+    return numberTwo
+   }else{
+    console.log( 'Both numbers are the same. Try diferents values.')
+   }
+}
+
+console.log(sum(5,5));
