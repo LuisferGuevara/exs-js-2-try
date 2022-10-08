329a3d2a3f4da9ce07844948fd30efdd0e2956bb
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-09 01:07:05 +0200

    add loop Iteration#2

--- loops/loops.js
+++ loops/loops.js
@@ -13,7 +13,31 @@ const products = [
 ];
 
 for (let index = 0; index < products.length; index++) {
-    if(products[index].includes('Camiseta'.toLowerCase)){
-        console.log(products[index]);
-    }
+  if (products[index].includes("Camiseta".toLowerCase)) {
+    console.log(products[index]);
+  }
 }
+
+// **Iteración #2: Condicionales avanzados**
+
+// Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y
+// añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas
+// compruébalo con un ***console.log***.
+
+const alumns = [
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
+
+
+for (const alumn of alumns) {
+  if ((alumn.T1 && alumn.T2) || (alumn.T1 && alumn.T3) || (alumn.T2 && alumn.T3))
+    alumn.isApproved = true;
+  else {
+    alumn.isApproved = false;
+  }
+}
+console.log(alumns);
