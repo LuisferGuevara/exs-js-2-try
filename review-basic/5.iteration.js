e101c03f82bf85955d6db41fd78129214dde27df
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-10 11:11:21 +0200

    add more solutions

--- review-basic/5.iteration.js
+++ review-basic/5.iteration.js
@@ -6,8 +6,35 @@
 //  resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca i
 //  nformación sobre la función de javascript **Math.random();**
 
-function rollDice(numberOfSides) {
-  return `Your lucky number is: ${Math.ceil(Math.random() * numberOfSides)}`;
-}
+//Opcion 1
+// function rollDice(numberOfSides) {
+//   return `Your lucky number is: ${Math.ceil(Math.random() * numberOfSides)}`;
+// }
 
+// console.log(rollDice(6));
+
+
+//Opciion 2
+
+function rollDice(sides) {
+  return Math.floor((Math.random() * sides) + 1)
+}
 console.log(rollDice(6));
+console.log(rollDice(20));
+
+/* Esto lo he hecho para comprobar que el código de arriba es fiable y
+que produce unos resultados "verdaderamente" aleatorios */
+let roll = '';
+function fairnessChecker(nSides, nTries) {
+  let stats = {}
+  for (let i = 0; i < nTries; i++) {
+      roll = "r" + rollDice(nSides)
+      if (!stats.hasOwnProperty(roll)) {
+          stats[roll] = 1
+      } else {
+          stats[roll]++
+      }
+  }
+  return stats
+}
+console.log(fairnessChecker(6, 6000))
